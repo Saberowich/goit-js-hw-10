aee231acf2a59f0f0b6ab47d18bc8e09534728d0
Bohdan Babych <babychbohdan0@gmail.com>
2024-02-18 19:18:46 +0100

    second commit

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,33 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+function createPromise(evt) {
+    evt.preventDefault();
+
+    const delay = form.delay.value;
+    const state = form.state.value;
+
+    const promise = new Promise((resolve, reject) => {
+        if (state === 'fulfilled') {
+            setTimeout(() => resolve(delay), delay);
+        } else if (state === 'rejected') {
+            setTimeout(() => reject(delay), delay);
+        }
+    }); 
+
+    promise.then((delay) => {
+    iziToast.success({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topCenter',
+    });
+})
+    .catch((delay) => {
+    iziToast.error({
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topCenter',
+    });
+    })
+}
+    form.addEventListener('submit', createPromise);
